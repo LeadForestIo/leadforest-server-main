50ea6c22b57900eb899f7aca85731f227ba757ec
LeadForestIO <Leadforest.io@gmail.com>
2024-01-08 08:19:51 -0500

    cors test

--- server.js
+++ server.js
@@ -104,4 +104,29 @@ db()
   .catch((err) => {
     console.log(`SERVER ERROR: ${err}`);
   });
+  // cors
+  const axios = require('axios');
+  
+  const port = 3001;
+  
+  app.use(express.json());
+  
+  app.get('/twitter', async (req, res) => {
+    try {
+      const response = await axios.get(`https://api.twitter.com${req.url}`, {
+        headers: {
+          Authorization: req.headers.authorization, // Forward Twitter API authorization header
+        },
+      });
+  
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(error.response.status).json(error.response.data);
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
   
